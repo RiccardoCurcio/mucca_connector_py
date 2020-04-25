9724f3a5e5fbc1b35d0ac79d33f7e746c914305b
riccardo <curcioriccardo@gmail.com>
2020-04-25 12:49:31 +0200

    new method tcp

--- mucca_connector.py
+++ mucca_connector.py
@@ -31,117 +31,125 @@ class mucca_connector:
         """Init."""
         pass
 
-    def serverHandler(self, port, buffersize, ptr=None):
-        """ServerHandler."""
-        with socket.socket(
-            socket.AF_INET,
-            socket.SOCK_DGRAM,
-            socket.IPPROTO_UDP
-        ) as ss:
-            host = ''
-            server_address = (host, port)
-            logging.log_info(
-                'PORT : {}'.format(port),
-                os.path.abspath(__file__),
-                sys._getframe().f_lineno
-            )
-            logging.log_info(
-                'BUFFER_SIZE : {}'.format(buffersize),
-                os.path.abspath(__file__),
-                sys._getframe().f_lineno
-            )
-            logging.log_info(
-                'PROTOCOL : UDP',
-                os.path.abspath(__file__),
-                sys._getframe().f_lineno
-            )
-            try:
-                ss.bind(server_address)
-            except OSError as emsg:
-                logging.log_error(
-                    'Socket Bind Error{}'.format(emsg),
-                    os.path.abspath(__file__),
-                    sys._getframe().f_lineno
-                )
-                ss.close()
-                sys.exit(1)
-            while True:
-                pid = os.fork()
-                if pid == 0:
-                    print("******************* child pid -> ", os.getpid())
-                    result = muccaChunckRecvfrom.run(ss, buffersize, logging)
-                    response = ptr(result["data"])
-                    muccaChunckSendTo.run(
-                        ss,
-                        buffersize,
-                        str(response),
-                        result["address"],
-                        logging
-                    )
-                    ss.close()
-                    os._exit(0)
-                else:
-                    print("****************** parent pid -> ", os.getpid())
-                    os.waitpid(0, 0)
-        return 0
+    # def serverHandler(self, port, buffersize, ptr=None):
+    #     """ServerHandler."""
+    #     with socket.socket(
+    #         socket.AF_INET,
+    #         socket.SOCK_DGRAM,
+    #         socket.IPPROTO_UDP
+    #     ) as ss:
+    #         host = ''
+    #         server_address = (host, port)
+    #         logging.log_info(
+    #             'PORT : {}'.format(port),
+    #             os.path.abspath(__file__),
+    #             sys._getframe().f_lineno
+    #         )
+    #         logging.log_info(
+    #             'BUFFER_SIZE : {}'.format(buffersize),
+    #             os.path.abspath(__file__),
+    #             sys._getframe().f_lineno
+    #         )
+    #         logging.log_info(
+    #             'PROTOCOL : UDP',
+    #             os.path.abspath(__file__),
+    #             sys._getframe().f_lineno
+    #         )
+    #         try:
+    #             ss.bind(server_address)
+    #         except OSError as emsg:
+    #             logging.log_error(
+    #                 'Socket Bind Error{}'.format(emsg),
+    #                 os.path.abspath(__file__),
+    #                 sys._getframe().f_lineno
+    #             )
+    #             ss.close()
+    #             sys.exit(1)
+    #         while True:
+    #             pid = os.fork()
+    #             if pid == 0:
+    #                 print("******************* child pid -> ", os.getpid())
+    #                 result = muccaChunckRecvfrom.run(ss, buffersize, logging)
+    #                 response = ptr(result["data"])
+    #                 muccaChunckSendTo.run(
+    #                     ss,
+    #                     buffersize,
+    #                     str(response),
+    #                     result["address"],
+    #                     logging
+    #                 )
+    #                 ss.close()
+    #                 os._exit(0)
+    #             else:
+    #                 print("****************** parent pid -> ", os.getpid())
+    #                 os.waitpid(0, 0)
+    #     return 0
+    #
+    # def clientUdp(self, port, ip, message, response_flag, buffersize):
+    #     """ClientUdp."""
+    #     response_rec = None
+    #     with socket.socket(
+    #         socket.AF_INET,
+    #         socket.SOCK_DGRAM,
+    #         socket.IPPROTO_UDP
+    #     ) as cs:
+    #         server_address = (ip, port)
+    #         c_message = bytes(message.encode())
+    #         try:
+    #             muccaChunckSendTo.run(
+    #                 cs,
+    #                 buffersize,
+    #                 str(c_message, "utf-8"),
+    #                 server_address,
+    #                 logging
+    #             )
+    #         except InterruptedError as emsg:
+    #             logging.log_error(
+    #                 'Interrupted signal error, sendto fail',
+    #                 os.path.abspath(__file__),
+    #                 sys._getframe().f_lineno
+    #             )
+    #         if response_flag != 0:
+    #             try:
+    #                 cs.settimeout(10.0)
+    #                 result = muccaChunckRecvfrom.run(cs, buffersize, logging)
+    #                 response_rec = result["data"]
+    #             except socket.timeout as emsg:
+    #                 response_rec = {
+    #                     "service": {
+    #                         "status": "500",
+    #                         "serviceName": "connector",
+    #                         "action": "NULL"
+    #                         },
+    #                     "head": {
+    #                         "Content-Type": "application/json; charset=utf-8",
+    #                         "Mucca-Service": "NULL"
+    #                         },
+    #                     "body": {
+    #                         "msg": "generic error"
+    #                     }
+    #                 }
+    #         else:
+    #             response_rec = {
+    #                 "service": {
+    #                     "status": "202",
+    #                     "serviceName": "connector",
+    #                     "action": "NULL"
+    #                     },
+    #                 "head": {
+    #                     "Content-Type": "application/json; charset=utf-8",
+    #                     "Mucca-Service": "NULL"
+    #                     },
+    #                 "body": {
+    #                     "msg": "Response 202 Accepted"
+    #                 }
+    #             }
+    #     return response_rec
 
-    def clientUdp(self, port, ip, message, response_flag, buffersize):
-        """ClientUdp."""
-        response_rec = None
-        with socket.socket(
-            socket.AF_INET,
-            socket.SOCK_DGRAM,
-            socket.IPPROTO_UDP
-        ) as cs:
-            server_address = (ip, port)
-            c_message = bytes(message.encode())
-            try:
-                muccaChunckSendTo.run(
-                    cs,
-                    buffersize,
-                    str(c_message, "utf-8"),
-                    server_address,
-                    logging
-                )
-            except InterruptedError as emsg:
-                logging.log_error(
-                    'Interrupted signal error, sendto fail',
-                    os.path.abspath(__file__),
-                    sys._getframe().f_lineno
-                )
-            if response_flag != 0:
-                try:
-                    cs.settimeout(10.0)
-                    result = muccaChunckRecvfrom.run(cs, buffersize, logging)
-                    response_rec = result["data"]
-                except socket.timeout as emsg:
-                    response_rec = {
-                        "service": {
-                            "status": "500",
-                            "serviceName": "connector",
-                            "action": "NULL"
-                            },
-                        "head": {
-                            "Content-Type": "application/json; charset=utf-8",
-                            "Mucca-Service": "NULL"
-                            },
-                        "body": {
-                            "msg": "generic error"
-                        }
-                    }
-            else:
-                response_rec = {
-                    "service": {
-                        "status": "202",
-                        "serviceName": "connector",
-                        "action": "NULL"
-                        },
-                    "head": {
-                        "Content-Type": "application/json; charset=utf-8",
-                        "Mucca-Service": "NULL"
-                        },
-                    "body": {
-                        "msg": "Response 202 Accepted"
-                    }
-                }
-        return response_rec
+    def tcpServerHandler():
+        """Tcp client."""
+        pass
+
+    def tcpClient(self, ports, ip, message, eventFlag, chunckSize):
+        """Tcp client."""
+        pass
